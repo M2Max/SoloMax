68f3ba4ea4ae2945831efd64f7d2b9689fc57457
Maximiliano Mamone <ilMamo437@gmail.com>
2025-04-30 08:47:57 +0000

    Removed security issue

--- src/firebase.ts
+++ src/firebase.ts
@@ -3,17 +3,17 @@ import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
+// Your web app's Firebase configuration using environment variables
 const firebaseConfig = {
-  apiKey: "AIzaSyA3NWiWV1y5ZaeojMRVb6wFPZx5v1r6ay8",
-  authDomain: "sitopersonale-e0bba.firebaseapp.com",
-  projectId: "sitopersonale-e0bba",
-  storageBucket: "sitopersonale-e0bba.firebasestorage.app",
-  messagingSenderId: "394361095279",
-  appId: "1:394361095279:web:f540f2019a1cb40380799a"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig as any);
 
 export default app;
